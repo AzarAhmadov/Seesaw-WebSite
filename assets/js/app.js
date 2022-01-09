998c4857d95b0caf694fd7354c36bc0e8c6fa0d1
azerahmedov <ehmedovh111@gmail.com>
2022-01-09 19:12:57 +0400

    duzenlemeler

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+function displayWatch(){
+    var t = new Date();
+    const day = document.querySelector('.day').innerHTML = t.getDay();
+    const hours = document.querySelector('.hours').innerHTML = t.getHours();
+    const secs = document.querySelector('.min').innerHTML = t.getMinutes();
+}
+
+window.load = displayWatch();
+setInterval(function(){
+displayWatch();
+},500);
