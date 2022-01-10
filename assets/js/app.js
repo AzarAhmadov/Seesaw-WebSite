1af976ce563d75f471d7edfe57a883f706f7446e
azerahmedov <ehmedovh111@gmail.com>
2022-01-11 00:21:18 +0400

    .

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,20 @@
-const openList = document.querySelector('.mobil-menu');
-const openMenu = document.querySelector('.open-men').addEventListener('click', () => {
-  openList.classList.toggle('active');
-});
+// const openList = document.querySelector('.mobil-menu');
+// const openMenu = document.querySelector('.open-men').addEventListener('click', () => {
+//   openList.classList.toggle('active');
+// });
 
 
-function displayWatch(){
-    var t = new Date();
-    const day = document.querySelector('.day').innerHTML = t.getDay();
-    const hours = document.querySelector('.hours').innerHTML = t.getHours();
-    const secs = document.querySelector('.min').innerHTML = t.getMinutes();
-}
+// function displayWatch(){
+//     var t = new Date();
+//     const day = document.querySelector('.day').innerHTML = t.getDay();
+//     const hours = document.querySelector('.hours').innerHTML = t.getHours();
+//     const secs = document.querySelector('.min').innerHTML = t.getMinutes();
+// }
 
-window.load = displayWatch();
-setInterval(function(){
-displayWatch();
-},500);
+// window.load = displayWatch();
+// setInterval(function(){
+// displayWatch();
+// },500);
 
 
 
