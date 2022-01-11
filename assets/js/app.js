497afa9c4d017a24ffc5b3a6ce1265f4768b31a3
azerahmedov <ehmedovh111@gmail.com>
2022-01-11 23:48:17 +0400

    duzenlemeler

--- assets/js/app.js
+++ assets/js/app.js
@@ -43,6 +43,23 @@ function format(num) {
   return (9 < num) ? num : "0" + num;
 }
 
+var mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 
 
